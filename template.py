0552f07107382671ad3d7f1f09578f096fceb63e
Malcolm-Hall <malcolm.r.hall@outlook.com>
2021-12-06 20:14:29 +0000

    add cli to template

--- template.py
+++ template.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 
 Day = str
 
@@ -28,11 +28,27 @@ def make_input_files(day: Day, suffix: str = ".txt"):
     make_file(challenge_input_path)
     make_file(example_input_path)
 
-def make_template(src_suffix: str = ".py", input_suffix: str = ".txt"):
-    for i in range(1, 26):
+def make_template(from_day: int = 1, to_day: int = 25, src_suffix: str = ".py", input_suffix: str = ".txt"):
+    for i in range(from_day, to_day + 1):
         day = format_day(i)
         make_source_file(day, src_suffix)
         make_input_files(day, input_suffix)
 
 if __name__ == '__main__':
-    make_template()
+    argv = sys.argv
+    kwarg_map = {
+        "_": "_",
+        "from_day": 1, 
+        "to_day": 25, 
+        "src_suffix": ".py", 
+        "input_suffix": ".txt"
+    }
+    for i, [key, arg] in enumerate(zip(kwarg_map.keys(), argv)):
+        parsed_arg = arg
+        if i == 0:
+            continue
+        elif i <= 2:
+            parsed_arg = int(arg)
+        kwarg_map[key] = parsed_arg
+    kwarg_map.pop("_")
+    make_template(**kwarg_map)

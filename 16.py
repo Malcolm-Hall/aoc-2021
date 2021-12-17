ced37222e55ec01bc17d5bd6be2804591ada6dc0
Malcolm Hall <malcolm.r.hall@outlook.com>
2021-12-17 09:43:59 +0000

    Add day 16 solutions

--- 16.py
+++ 16.py
@@ -1,126 +1,118 @@
 from util import Bot
-import functools
+from io import StringIO
+from math import prod
 
 example_input_file = "./input/16-example.txt" 
 challenge_input_file = "./input/16.txt" 
 bot = Bot(example_input_file, challenge_input_file) 
 
-hex_to_bin = {
-    "0": "0000",
-    "1": "0001",
-    "2": "0010",
-    "3": "0011",
-    "4": "0100",
-    "5": "0101",
-    "6": "0110",
-    "7": "0111",
-    "8": "1000",
-    "9": "1001",
-    "A": "1010",
-    "B": "1011",
-    "C": "1100",
-    "D": "1101",
-    "E": "1110",
-    "F": "1111",
-}
 def parse_input(filename):
+    data = []
     with open(filename) as f:
-        packets = []
         for line in f:
-            hex_packet = line.strip()
-            bin_packet = "".join([hex_to_bin[digit] for digit in hex_packet])
-            packets.append(bin_packet)
-        return packets
+            data.append(line.strip())
+    return data
 
-def parse_header(packet):
-    version = packet[:3]
-    type_id = packet[3:6]
-    rest = packet[6:]
-    return version, type_id, rest
+class Packet:
+    def __init__(self, hex_input=None, bin_input=None, stream=None):
+        if not (hex_input or bin_input or stream):
+            raise TypeError('No input provided')
+        if hex_input:
+            bin_input = ''.join(f'{int(char, 16):04b}' for char in hex_input)
+        if bin_input:
+            stream = StringIO(bin_input)
+        self.stream = stream
+        self.version = int(self.stream.read(3), 2)
+        self.type_id= int(self.stream.read(3), 2)
+        self.subpackets = [] if self.type_id == 4 else self.read_subpackets()
+        self.value = {
+            0: self.sum,
+            1: self.prod,
+            2: self.min,
+            3: self.max,
+            4: self.read_literal,
+            5: self.greater_than,
+            6: self.less_than,
+            7: self.equal
+        }[self.type_id]()
+        self.versions_sum = sum(self.versions())
 
-def parse_100(rest):
-    content = ''
-    while rest[0] == '1':
-        content += rest[:5]
-        rest = rest[5:]
-    content += rest[:5]
-    rest = rest[5:]
-    return rest, int(content,2)
+    def read_literal(self):
+        heading = literal = ''
+        while heading != '0':
+            heading = self.stream.read(1)
+            literal += self.stream.read(4)
+        return int(literal, 2)
 
+    def read_subpackets(self):
+        length_type_id = self.stream.read(1)
+        if length_type_id == '0':
+            total_length = int(self.stream.read(15), 2)
+            end = self.stream.tell() + total_length
+            subpackets = []
+            while self.stream.tell() < end:
+                subpackets.append(Packet(stream=self.stream))
+        else:
+            num_subpackets = int(self.stream.read(11), 2)
+            subpackets = [Packet(stream=self.stream) for _ in range(num_subpackets)]
+        return subpackets 
 
-def parse_subpackets(rest):
-    version_sum = 0
-    length_id = rest[0]
-    rest = rest[1:]
-    contents = []
-    if length_id == "0":
-        length = rest[:15]
-        rest = rest[15:]
-        target = int(length, 2)
-        while target > 0:
-            len_before = len(rest)
-            rest, vs, content = decode_packet(rest)
-            version_sum += vs
-            contents.append(content)
-            target -= len_before - len(rest)
-    else:
-        length = rest[:11]
-        rest = rest[11:]
-        for _ in range(int(length, 2)):
-            rest, vs, content = decode_packet(rest)
-            version_sum += vs
-            contents.append(content)
-    return rest, version_sum, contents
+    def sum(self):
+        return sum(subpacket.value for subpacket in self.subpackets)
 
-def decode_packet(packet):
-    version, type_id, rest = parse_header(packet)
-    version_sum = int(version, 2)
-    if type_id == '100':
-        rest, content = parse_100(rest)
-        return rest, version_sum, content
-    else:
-        rest, vs, contents = parse_subpackets(rest)
-        version_sum += vs
+    def prod(self):
+        return prod(subpacket.value for subpacket in self.subpackets)
+
+    def min(self):
+        return min(subpacket.value for subpacket in self.subpackets)
+
+    def max(self):
+        return max(subpacket.value for subpacket in self.subpackets)
+
+    def greater_than(self):
+        first, second, *_ = self.subpackets
+        return int(first.value > second.value)
+
+    def less_than(self):
+        first, second, *_ = self.subpackets
+        return int(first.value < second.value)
+
+    def equal(self):
+        first, second, *_ = self.subpackets
+        return int(first.value == second.value)
+                
+    def versions(self):
+        yield self.version
+        for subpacket in self.subpackets:
+            yield from subpacket.versions()
     
-    match type_id:
-        case '000':
-            content = sum(contents)
-        case '001':
-            content = functools.reduce(lambda a, b : a*b, contents)
-        case '010':
-            content = min(contents)
-        case '011':
-            content = max(contents)
-        case '101':
-            content = int(contents[0] > contents[1])
-        case '110':
-            content = int(contents[0] < contents[1])
-        case '111':
-            content = int(contents[0] == contents[1])
-    
-    return rest, version_sum, content
-    
+    def __repr__(self):
+        return f'Packet(version={self.version}, type_id={self.type_id}, value={self.value}, subpackets={self.subpackets})'
 
-def version_number_sum(_, packet):
-    _, version_sum, _ = decode_packet(packet)
-    return version_sum
 
-def content_result(_, packet):
-    _, _, content = decode_packet(packet)
-    return content
+def read_packet(filename, packet_index):
+    data = parse_input(filename)
+    packet = Packet(data[packet_index])
+    return packet
+
+def version_number_sum(filename, packet_index=0):
+    packet = read_packet(filename, packet_index)
+    return packet.versions_sum
+
+def content_result(filename, packet_index=0):
+    packet = read_packet(filename, packet_index)
+    return packet.value
 
 expected_version_number_sums = (6, 9, 14, 16, 12, 23, 31)
 expected_content_results = (3, 54, 7, 9, 1, 0, 0, 1)
 
-
-example_packets = parse_input(example_input_file)
-[challenge_packet] = parse_input(challenge_input_file)
-
 for i, expected_version_sum in enumerate(expected_version_number_sums):
-    bot.check_example(expected_version_sum, version_number_sum, [example_packets[i]])
+    if not bot.check_example(expected_version_sum, version_number_sum, [i]):
+        exit(1)
+print(version_number_sum(challenge_input_file))
 
 for j, expected_result in enumerate(expected_content_results):
-    bot.check_example(expected_result, content_result, [example_packets[len(expected_version_number_sums) + j]])
+    if not bot.check_example(expected_result, content_result, [len(expected_version_number_sums) + j]):
+        exit(1)
+print(content_result(challenge_input_file))
 
-# print(version_number_sum(None, challenge_packet))
-print(content_result(None, challenge_packet))
